e88280b29dde487ec7d2a404a19321d855913650
Qinyi Cai <CaiQinyi@foxmail.com>
2018-07-14 23:19:05 +0800

    先开个坑……

---
+++ ActiveAttack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 主动攻击:发出水波攻击范围内的敌人,没有面向限制,就是周围一圈的敌人都可以攻击.
+/// </summary>
+public class ActiveAttack : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
